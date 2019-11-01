b0aea66852dd1f4f1b6e8c2319257a65c6042d06
ASEDOS999 <kuruzov2014@mail.ru>
2019-11-01 16:37:38 +0300

    Successfull parsing

--- xml_parsing.py
+++ xml_parsing.py
@@ -1,16 +1,31 @@
 import xml.etree.ElementTree as ET
 
+def extract_inform_from_chain(chain):
+	data = dict()
+	keys = ['answer', 'anaphor']
+	for ind, child in enumerate(chain):
+		attr = child.attrib
+		text = [cont.text for cont in child][0]
+		data[keys[ind]] = {'text' : text, 'attr' : attr}
+	return data
+
 def extract_dataset(name_file):
 	tree = ET.parse(name_file)
 	root = tree.getroot()
+	res = dict()
 	for doc in root:
-		print(dir(doc))
-		print(doc.tag, doc.attrib)
+		name_doc = doc.attrib['file']
+		cur_list = []
 		for chain in doc:
-			print('new_chain')
-			for child in chain:
-				print(child.tag, child.attrib, child.text)
-				for cont in child:
-					print(cont.text)
+			cur_list.append(extract_inform_from_chain(chain))
+		res[name_doc] = cur_list
+	return res
+
+
 if __name__ == '__main__':
-	extract_dataset('LearnSet/anaph_new.xml')
+	dataset = extract_dataset('LearnSet/anaph_new.xml')
+	print('Documents Number', len(dataset.keys()))
+	s = 0
+	for i in dataset:
+		s += len(i)
+	print('Chains Number', s)
