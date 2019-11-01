5b4271595bae03b6cb20ae613d0916e0cc6281ba
ASEDOS999 <kuruzov2014@mail.ru>
2019-11-01 11:16:58 +0300

    Research structure

--- xml_parsing.py
+++ xml_parsing.py
@@ -4,5 +4,13 @@ def extract_dataset(name_file):
 	tree = ET.parse(name_file)
 	root = tree.getroot()
 	for doc in root:
-		for child in doc:
-			print(child.tag, child.attr)
+		print(dir(doc))
+		print(doc.tag, doc.attrib)
+		for chain in doc:
+			print('new_chain')
+			for child in chain:
+				print(child.tag, child.attrib, child.text)
+				for cont in child:
+					print(cont.text)
+if __name__ == '__main__':
+	extract_dataset('LearnSet/anaph_new.xml')
