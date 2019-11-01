5f266cd356600e919827faf9a0a9d3cf1d110662
ASEDOS999 <kuruzov2014@mail.ru>
2019-11-01 20:51:50 +0300

    Add function for to get json result

--- Parser/some_reparser.py
+++ Parser/some_reparser.py
@@ -17,44 +17,47 @@ class Configuration:
 		self.cgiPath = str()
 
 
-configParser = ConfigParser.ConfigParser()
-configParser.read("intellection_wrapper.cfg")
-config = Configuration()
-config.webAddress = configParser.get("Common", "webAddress")
-config.cgiPath = configParser.get("Common", "cgiPath")
+def parse(text):
+	configParser = ConfigParser.ConfigParser()
+	configParser.read("intellection_wrapper.cfg")
+	config = Configuration()
+	config.webAddress = configParser.get("Common", "webAddress")
+	config.cgiPath = configParser.get("Common", "cgiPath")
 
-sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
-sys.stderr = codecs.getwriter('utf-8')(sys.stderr.detach())
+	sys.stdout = codecs.getwriter('utf-8')(sys.stdout.detach())
+	sys.stderr = codecs.getwriter('utf-8')(sys.stderr.detach())
 
-sentence = "Мама мыла раму."
-d_data = {
-	"jsonrpc": "2.0",
-	"method": "processText",
-	"params": {
-		"format": "json",
-		"argIdent": 0,
-		"text": sentence,
-		"syntax_parser": 1,
-		"semsyn": False
-	},
-	"id": "1601908510"
-}
-send_data = json.dumps(d_data, ensure_ascii = True)
-send_data = send_data.encode()
+	d_data = {
+		"jsonrpc": "2.0",
+		"method": "processText",
+		"params": {
+			"format": "json",
+			"argIdent": 0,
+			"text": text,
+			"syntax_parser": 1,
+			"semsyn": False
+		},
+		"id": "1601908510"
+	}
+	send_data = json.dumps(d_data, ensure_ascii = True)
+	send_data = send_data.encode()
 
-headers = {"Content-type": "text/json"}
-params = urllib.parse.urlencode({"PROTOCOL": "JSONRPC"})
-conn = httplib.HTTPConnection(config.webAddress)
-conn.request("POST", config.cgiPath + "?" + params, send_data, headers)
-response = conn.getresponse()
-data = response.read()
+	headers = {"Content-type": "text/json"}
+	params = urllib.parse.urlencode({"PROTOCOL": "JSONRPC"})
+	conn = httplib.HTTPConnection(config.webAddress)
+	conn.request("POST", config.cgiPath + "?" + params, send_data, headers)
+	response = conn.getresponse()
+	data = response.read()
 
-conn.close()
-data = data.decode('utf-8')
-if response.status != 200:
-	raise Exception("Got status {} because '{}'".format(response.status, response.reason))
+	conn.close()
+	data = data.decode('utf-8')
+	if response.status != 200:
+		raise Exception("Got status {} because '{}'".format(response.status, response.reason))
 
-json_format = json.loads(data)
-json_result = json.loads(base64.b64decode(json_format["result"]["boost_serialization"]["datastream"]["content"]).decode('utf-8'))
+	json_format = json.loads(data)
+	return json.loads(base64.b64decode(json_format["result"]["boost_serialization"]["datastream"]["content"]).decode('utf-8'))
 
-print(json_result)
+if __name__ == '__main__':
+	sentence = "Мама мыла раму."
+	json_result = parse(sentence)
+	print(json_result)
