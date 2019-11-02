691fa1a9ad88065b98c3559672d1a5a0a34ab5c5
ASEDOS999 <kuruzov2014@mail.ru>
2019-11-02 14:10:46 +0300

    Add function for to create dataset according our method

--- Train/dataset_creating.py
+++ Train/dataset_creating.py
@@ -11,11 +11,25 @@ def get_all_files(folder, files = None):
 	for i in os.listdir(folder):
 		cur_name = folder + i
 		if os.path.isdir(cur_name):
-			files = get_all_files(cur_name, files)
+			files = get_all_files(cur_name + '/', files)
 		else:
 			files.append(cur_name)
 	return files
 
+def get_dataset(folder):
+	files = (get_all_files(folder))
+	dataset = dict()
+	for file in files:
+		f = open(file)
+		text  = f.read()
+		dataset[file] = anaphora_resolution.get_antecedent_anaphor(text)
+		f.close()
+	return dataset
+
+
 if __name__ == '__main__':
-	files = (get_all_files('../LearnSet/TestSet/'))
+	folder = '../LearnSet/TestSet/'
+	my_dataset = get_dataset(folder)
+	dataset_from_xml = xml_parsing.extract_dataset('../LearnSet/anaph_new.xml')
 	print(files[:10])
+	print(dataset_from_xml)
