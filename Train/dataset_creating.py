890989e38a1c7e0a10b6b404f4afc0dbfe6415b9
ASEDOS999 <kuruzov2014@mail.ru>
2019-11-02 15:21:57 +0300

    Add threading

--- Train/dataset_creating.py
+++ Train/dataset_creating.py
@@ -3,7 +3,8 @@ import os
 sys.path.append('..')
 import xml_parsing
 import anaphora_resolution
-
+import pickle
+import threading
 
 def get_all_files(folder, files = None):
 	if files is None:
@@ -16,20 +17,40 @@ def get_all_files(folder, files = None):
 			files.append(cur_name)
 	return files
 
-def get_dataset(folder):
-	files = (get_all_files(folder))
-	dataset = dict()
-	for file in files:
-		f = open(file)
+import time
+def get_dataset(files):
+	def process_text(file, dataset):
+		f = open(file, 'r')
 		text  = f.read()
 		dataset[file] = anaphora_resolution.get_antecedent_anaphor(text)
 		f.close()
+	dataset = dict()
+	t = []
+	s = time.time()
+	for file in files:
+		t.append(threading.Thread(target = process_text, args = (file, dataset)))
+	for thread in t:
+		thread.start()
+	for thread in t:
+		thread.join()
+	print(time.time()-s)
 	return dataset
 
-
 if __name__ == '__main__':
-	folder = '../LearnSet/TestSet/'
-	my_dataset = get_dataset(folder)
+	folder = '../LearnSet/AnaphFiles/'
+	print('Number Of Files',len(get_all_files(folder)))
 	dataset_from_xml = xml_parsing.extract_dataset('../LearnSet/anaph_new.xml')
-	print(files[:10])
-	print(dataset_from_xml)
+	files = dataset_from_xml.keys()
+	files = [folder + i for i in files]
+	print('Number Of Files In xml-file', len(files))
+	name_pickle = 'our_dataset.pickle'
+	try:
+		f = open(name_pickle, 'rb')
+		my_dataset = pickle.load(f)
+		f.close()
+	except:
+		my_dataset = get_dataset(files)
+		f = open(name_pickle, 'wb')
+		pickle.dump(my_dataset, f)
+		f.close()
+
